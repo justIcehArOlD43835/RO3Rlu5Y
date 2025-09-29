37f38e7ee4f9562fa912fb38e840d37f88a45359
justIcehArOlD43835 <nervous.grazing.0e@icloud.com>
2025-09-29 10:45:26 +0200

    Setup new 测试执行调度器 feature

---
+++ test_scheduler_0929_1645_ljh.rb
@@ -0,0 +1,51 @@
+# 代码生成时间: 2025-09-29 16:45:25
+# test_scheduler.rb
+#
+# A simple test scheduler using the Grape framework in Ruby.
+# This scheduler is designed to run test scripts at specified intervals.
+
+require 'grape'
+require 'rufus-scheduler'
+
+# Define the TestScheduler API using Grape
+class TestScheduler < Grape::API
+  # Version of the API
+  VERSION = 'v1'
+  prefix :api
+  format :json
+  default_format :json
+
+  # Helper method to schedule tests
+  def self.schedule_test(test_name, interval)
+    Rufus::Scheduler.new do
+      every interval do
+        run_test(test_name)
+      end
+    end
+  end
+
+  # Endpoint to start the test scheduler
+  get do
+    # Get the test name and interval from params
+    test_name = params[:test_name]
+    interval = params[:interval].to_i
+
+    # Validate inputs
+    if test_name.nil? || interval <= 0
+      error!('Invalid input', 400)
+    end
+
+    # Schedule the test
+    scheduler = TestScheduler.schedule_test(test_name, interval)
+    { message: "Test '#{test_name}' scheduled every #{interval} seconds." }
+  rescue Rufus::Scheduler::InvalidIntervalError => e
+    error!('Invalid interval', 400)
+  end
+
+  private
+
+  # Method to run the actual test
+  def self.run_test(test_name)
+    # Placeholder for actual test logic
+    # This is where you would include the logic to run your tests
+    puts 
